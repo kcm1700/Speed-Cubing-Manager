095abaca9858bfd8bf9ec30ad3c31c7bc0491484
cmkim <cmkim@innotive.com>
2011-09-01 16:43:47 +0900

    XP에서 실행 안되는 문제 고치기 작업

--- Speed-Cubing-Manager/D3DRenderer.cpp
+++ Speed-Cubing-Manager/D3DRenderer.cpp
@@ -5,8 +5,24 @@
 CD3DRenderer::CD3DRenderer(HWND hWnd)
 {
 	HRESULT hr;
-	hr = Direct3DCreate9Ex(D3D_SDK_VERSION, &m_pD3D9ex);
-	if ( FAILED(hr) ) return;
+	HMODULE hlib = LoadLibrary(TEXT("d3d9.dll"));
+	if(hlib == nullptr) return;
+
+	HRESULT (*pDirect3DCreate9Ex)(UINT, IDirect3D9Ex **) = 
+		reinterpret_cast<HRESULT (*)(UINT, IDirect3D9Ex **)>(GetProcAddress(hlib, "Direct3DCreate9Ex"));
+
+	if(pDirect3DCreate9Ex != nullptr)
+	{
+		hr = (*pDirect3DCreate9Ex)(D3D_SDK_VERSION, &m_pD3D9ex);
+		if( FAILED(hr) ) return;
+		hr = m_pD3D9ex->QueryInterface(IID_IDirect3D9, reinterpret_cast<void **>(&m_pD3D9));
+		if( FAILED(hr) ) return;
+	}
+	else
+	{
+		m_pD3D9 = Direct3DCreate9(D3D_SDK_VERSION);
+		if(m_pD3D9 == nullptr) return;
+	}
 
 	D3DDEVTYPE devType = D3DDEVTYPE_HAL;
 	if ( /* ¿É¼Ç */ false) {
@@ -24,8 +40,8 @@ CD3DRenderer::CD3DRenderer(HWND hWnd)
 	d3dpp.BackBufferWidth = 1; /* TODO: */
 	d3dpp.BackBufferHeight = 1; /* TODO: */
 	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
-	hr = m_pD3D9ex->CreateDeviceEx(D3DADAPTER_DEFAULT, devType, hWnd, 
-		createOption, &d3dpp, NULL, &m_pD3DDevice9);
+	hr = m_pD3D9->CreateDevice(D3DADAPTER_DEFAULT, devType, hWnd, 
+		createOption, &d3dpp, &m_pD3DDevice9);
 }
 
 #define SAFE_RELEASE(pObj) \
@@ -37,6 +53,7 @@ CD3DRenderer::CD3DRenderer(HWND hWnd)
 CD3DRenderer::~CD3DRenderer(void)
 {
 	SAFE_RELEASE(m_pD3DDevice9);
+	SAFE_RELEASE(m_pD3D9);
 	SAFE_RELEASE(m_pD3D9ex);
 }
 
--- Speed-Cubing-Manager/D3DRenderer.h
+++ Speed-Cubing-Manager/D3DRenderer.h
@@ -1,7 +1,6 @@
 #ifndef D3DRENDERER_H__
 #define D3DRENDERER_H__
-#include <d3d9.h>
-#include <d3dx9.h>
+
 class CD3DRenderer
 {
 public:
@@ -9,7 +8,8 @@ public:
 	virtual ~CD3DRenderer(void);
 private:
 	CD3DRenderer(void);
+	LPDIRECT3D9 m_pD3D9;
 	LPDIRECT3D9EX m_pD3D9ex;
-	LPDIRECT3DDEVICE9EX m_pD3DDevice9;
+	LPDIRECT3DDEVICE9 m_pD3DDevice9;
 };
 #endif
